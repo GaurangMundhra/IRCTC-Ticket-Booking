d012ad06fcdb265f986caff53e4426373179407b
Gaurang Mundhra <gaurangmundhra51@gmail.com>
2026-01-04 22:42:26 +0530

    Successfully built Ticket Booking App

--- app/src/main/java/com/ticketbooking/App.java
+++ app/src/main/java/com/ticketbooking/App.java
@@ -3,12 +3,108 @@
  */
 package com.ticketbooking;
 
+import com.ticketbooking.entities.Train;
+import com.ticketbooking.entities.User;
+import com.ticketbooking.services.UserBookingService;
+import com.ticketbooking.util.UserServiceUtil;
+
+import java.io.IOException;
+import java.util.*;
+
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        System.out.println("Running Train Booking System");
+        Scanner scanner = new Scanner(System.in);
+        int option = 0;
+
+        UserBookingService userBookingService;
+
+        try{
+            userBookingService = new UserBookingService();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+            return;
+        }
+
+        while(option!=7){
+            System.out.println("Choose option");
+            System.out.println("1. Sign up");
+            System.out.println("2. Login");
+            System.out.println("3. Fetch Bookings");
+            System.out.println("4. Search Trains");
+            System.out.println("5. Book a Seat");
+            System.out.println("6. Cancel my Booking");
+            System.out.println("7. Exit the App");
+            option = scanner.nextInt();
+            Train trainSelectedForBooking = new Train();
+            switch (option){
+                case 1:
+                    System.out.println("Enter the username to signup");
+                    String nameToSignUp = scanner.next();
+                    System.out.println("Enter the password to signup");
+                    String passwordToSignUp = scanner.next();
+                    User userToSignup = new User(nameToSignUp, passwordToSignUp, UserServiceUtil.hashedPassword(passwordToSignUp), new ArrayList<>(), UUID.randomUUID().toString());
+                    userBookingService.signUp(userToSignup);
+                    break;
+                case 2:
+                    System.out.println("Enter the username to Login");
+                    String nameToLogin = scanner.next();
+                    System.out.println("Enter the password to signup");
+                    String passwordToLogin = scanner.next();
+                    User userToLogin = new User(nameToLogin, passwordToLogin, UserServiceUtil.hashedPassword(passwordToLogin), new ArrayList<>(), UUID.randomUUID().toString());
+                    try{
+                        userBookingService = new UserBookingService(userToLogin);
+                    }catch (IOException ex){
+                        return;
+                    }
+                    break;
+                case 3:
+                    System.out.println("Fetching your bookings");
+                    userBookingService.fetchBookings();
+                    break;
+                case 4:
+                    System.out.println("Type your source station");
+                    String source = scanner.next();
+                    System.out.println("Type your destination station");
+                    String dest = scanner.next();
+                    List<Train> trains = userBookingService.getTrains(source, dest);
+                    int index = 1;
+                    for (Train t: trains){
+                        System.out.println(index+" Train id : "+t.getTrainId());
+                        for (Map.Entry<String, String> entry: t.getStationTimes().entrySet()){
+                            System.out.println("station "+entry.getKey()+" time: "+entry.getValue());
+                        }
+                    }
+                    System.out.println("Select a train by typing 1,2,3...");
+                    trainSelectedForBooking = trains.get(scanner.nextInt());
+                    break;
+                case 5:
+                    if (trainSelectedForBooking == null) {
+                        System.out.println("Please select a train first (option 4).");
+                        break;
+                    }
+
+                    List<List<Integer>> seats =
+                            userBookingService.fetchSeats(trainSelectedForBooking);
+
+                    if (seats == null) {
+                        System.out.println("Seat data not available.");
+                        break;
+                    }
+
+                    System.out.println("Select a seat out of these seats");
+                    for (List<Integer> row : seats) {
+                        for (Integer val : row) {
+                            System.out.print(val + " ");
+                        }
+                        System.out.println();
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
--- app/src/main/java/com/ticketbooking/services/UserBookingService.java
+++ app/src/main/java/com/ticketbooking/services/UserBookingService.java
@@ -2,12 +2,16 @@ package com.ticketbooking.services;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ticketbooking.entities.Train;
 import com.ticketbooking.entities.User;
+import com.ticketbooking.util.UserServiceUtil;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Scanner;
 
 public class UserBookingService {
 
@@ -17,12 +21,20 @@ public class UserBookingService {
 
     private ObjectMapper objectMapper = new ObjectMapper();
 
-    private static final String USERS_PATH = "java/com/ticketbooking/localDb/users.json";
+    private static final String USERS_PATH = "C:/Users/gaura/OneDrive/Desktop/IRCTC/app/src/main/java/com/ticketbooking/localDb/users.json";
 
-    public UserBookingService(User user1) throws IOException {
-        this.user = user1;
-        File users = new File(USERS_PATH);
-        userList = objectMapper.readValue(users, new TypeReference<List<User>>() {});
+    public UserBookingService(User user) throws IOException {
+        this.user = user;
+        loadUserListFromFile();
+    }
+
+    public UserBookingService() throws IOException {
+        loadUserListFromFile();
+    }
+
+    private void loadUserListFromFile() throws IOException {
+
+        userList = objectMapper.readValue(new File(USERS_PATH), new TypeReference<List<User>>() {});
     }
 
     public Boolean loginUser(){
@@ -42,5 +54,73 @@ public class UserBookingService {
         }
     }
 
+    private void saveUserListToFile() throws IOException
+    {
+        File userFile = new File(USERS_PATH);
+        objectMapper.writeValue(userFile,userList);
+    }
+
+    public void fetchBookings(){
+        user.printTickets();
+    }
+
+    public Boolean cancelBooking(String ticketId){
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter the ticket to cancel:");
+        ticketId = s.next();
+
+        if(ticketId == null || ticketId.isEmpty()){
+            System.out.println("TicketId cannot be null or empty");
+            return Boolean.FALSE;
+        }
+
+        String finalticketId = ticketId;
+        boolean removed = user.getTicketsBooked().removeIf(ticket -> ticket.getTicketId().equals(finalticketId));
+
+        if(removed){
+            System.out.println("Ticket with Id" + ticketId + "has been cancelled");
+            return Boolean.TRUE;
+        }
+        else {
+            System.out.println("No ticket found with Id" + ticketId);
+            return Boolean.FALSE;
+        }
+    }
+
+    public List<Train> getTrains(String source, String destination){
+        try{
+            TrainService trainService = new TrainService();
+            return trainService.searchTrains(source, destination);
+        }catch (IOException ex){
+            return new ArrayList<>();
+        }
+
+    }
+
+    public List<List<Integer>> fetchSeats(Train train){
+        return train.getSeats();
+    }
+
+    public Boolean bookTrainSeat(Train train, int row, int seat) {
+        try{
+            TrainService trainService = new TrainService();
+            List<List<Integer>> seats = train.getSeats();
+            if (row >= 0 && row < seats.size() && seat >= 0 && seat < seats.get(row).size()) {
+                if (seats.get(row).get(seat) == 0) {
+                    seats.get(row).set(seat, 1);
+                    train.setSeats(seats);
+                    trainService.addTrain(train);
+                    return true; // Booking successful
+                } else {
+                    return false; // Seat is already booked
+                }
+            } else {
+                return false; // Invalid row or seat index
+            }
+        }catch (IOException ex){
+            return Boolean.FALSE;
+        }
+    }
 
 }
